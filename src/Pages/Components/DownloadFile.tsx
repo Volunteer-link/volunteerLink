98cb9afc18ea3ce8eaa3044fdd78120c84217270
Le Son <lson999920.work@gmail.com>
2025-04-03 16:58:19 +0700

    fix UI 1

--- src/Pages/Components/DownloadFile.tsx
+++ src/Pages/Components/DownloadFile.tsx
@@ -21,7 +21,7 @@ const DownloadFile: React.FC<{ fileUrl: string }> = ({ fileUrl }) => {
       download={getFileNameFromUrl(fileUrl)}
       className="py-2 px-2 inline-block hover:scale-105 transition-all text-primary-color"
     >
-      Tải xuống PDF
+      {getFileNameFromUrl(fileUrl)}
     </a>
   );
 };
